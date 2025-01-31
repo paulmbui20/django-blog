069ab09af8fb56a8997e5827be0eca5a1ec94554
MP <paulmbui20@gmail.com>
2025-01-31 15:56:14 +0300

    fixed db not compatible

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
 ]
 INSTALLED_APPS = [
     'django.contrib.sitemaps', # for sitemaps
-
+    'django_daisy', # for admin styling
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -233,4 +233,28 @@ CKEDITOR_5_CONFIGS = {
     }
 }
 
+DAISY_SETTINGS = {
+    'SITE_TITLE': 'Django Blog Admin',  # The title of the site
+    'SITE_HEADER': 'Administration',  # Header text displayed in the admin panel
+    'INDEX_TITLE': 'Hi, welcome to the admin ',  # The title for the index page of dashboard
+    # 'SITE_LOGO': '',  # Path to the logo image displayed in the sidebar
+    'EXTRA_STYLES': [],  # List of extra stylesheets to be loaded in base.html (optional)
+    'EXTRA_SCRIPTS': [],  # List of extra script URLs to be loaded in base.html (optional)
+    'LOAD_FULL_STYLES': False,  # If True, loads full DaisyUI components in the admin (useful if you have custom template overrides)
+    'SHOW_CHANGELIST_FILTER': False,  # If True, the filter sidebar will open by default on changelist views
+    'DONT_SUPPORT_ME': True, # Hide github link in sidebar footer
+    'SIDEBAR_FOOTNOTE': 'Developed by paulmbui.co.ke', # add footnote to sidebar
+    'APPS_REORDER': {
+        # Custom configurations for third-party apps that can't be modified directly in their `apps.py`
+        'auth': {
+            'icon': 'fa-solid fa-person-military-pointing',  # FontAwesome icon for the 'auth' app
+            'name': 'Authentication',  # Custom name for the 'auth' app
+            'hide': False,  # Whether to hide the 'auth' app from the sidebar (set to True to hide)
+            'divider_title': "Auth",  # Divider title for the 'auth' section
+        },
+        'social_django': {
+            'icon': 'fa-solid fa-users-gear',  # Custom FontAwesome icon for the 'social_django' app
+        },
+    },
+}
 
